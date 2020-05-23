509263aaf0109a43aabc5b45c78043d4d147d538
lexlopez-dev <53419066+lexlopez-dev@users.noreply.github.com>
2020-05-22 21:44:30 -0700

    updates to security and user registration

--- project-management/src/main/java/com/jrp/pma/security/SecurityConfiguration.java
+++ project-management/src/main/java/com/jrp/pma/security/SecurityConfiguration.java
@@ -1,43 +1,41 @@
 package com.jrp.pma.security;
 
-import javax.activation.DataSource;
-
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
-import org.springframework.security.crypto.password.NoOpPasswordEncoder;
-import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 
 @Configuration
 @EnableWebSecurity
 public class SecurityConfiguration extends WebSecurityConfigurerAdapter {
 	
 	@Autowired
-	DataSource dataSource;
+	javax.sql.DataSource dataSource;
+	
+	@Autowired
+	BCryptPasswordEncoder bCryptEncoder;
 	
 	@Override
 	protected void configure(AuthenticationManagerBuilder auth) throws Exception {
-		auth.inMemoryAuthentication()
-		.withUser("myuser")
-			.password("pass")
-			.roles("USER")
-		.and()
-		.withUser("managerUser")
-		.password("pass3")		.roles("ADMIN");
+		auth.jdbcAuthentication()
+		.usersByUsernameQuery("select username, password, enabled " +
+		"from user_accounts where username = ?")
+		.authoritiesByUsernameQuery("select username, role " + 
+		"from user_accounts where username = ?")
+		.dataSource(dataSource)
+		.passwordEncoder(bCryptEncoder);
+		
 	}
 	
-	@Bean
-	public PasswordEncoder getPasswordEncoder() {
-		return NoOpPasswordEncoder.getInstance();
-	}
 	
 	@Override
 	protected void configure(HttpSecurity http) throws Exception{
 		http.authorizeRequests()
-			.antMatchers("/").authenticated().and().formLogin();
+			.antMatchers("/", "/**").permitAll()
+			.and()
+			.formLogin();
 	}
 }
